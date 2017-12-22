33fdc1bb90b2cfc70028df55811bea7b2611cbb5
Dan White <danjwh2@gmail.com>
2017-12-21 21:47:39 -0500

    Finished INoteService and NoteServiceImpl

---
+++ src/main/java/com/example/myapp/service/NoteServiceImpl.java
@@ -0,0 +1,64 @@
+package com.example.myapp.service;
+
+import com.example.myapp.dao.INoteDao;
+import com.example.myapp.domain.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class NoteServiceImpl implements INoteService {
+
+    private INoteDao noteDao;
+
+    @Autowired
+    public NoteServiceImpl(INoteDao noteDao) {
+        this.noteDao = noteDao;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Note> findAll() {
+        return noteDao.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Note> findRecent() {
+        return noteDao.findRecent();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<Note> findRecent(int count) {
+        return noteDao.findRecent(count);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public Note findOne(long id) {
+        return noteDao.findOne(id);
+    }
+
+    @Transactional
+    @Override
+    public Note save(Note note) {
+        return noteDao.save(note);
+    }
+
+    @Transactional
+    @Override
+    public void delete(long id) {
+        noteDao.delete(id);
+    }
+
+    @Transactional
+    @Override
+    public long count() {
+        return noteDao.count();
+    }
+
+
+}
