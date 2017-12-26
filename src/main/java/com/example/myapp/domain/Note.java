77f6508cb28dbbed135957d007c7cedcb5fc8fb6
Dan White <danjwh2@gmail.com>
2017-12-26 18:22:12 -0500

    Added @Column to id in Note

--- src/main/java/com/example/myapp/domain/Note.java
+++ src/main/java/com/example/myapp/domain/Note.java
@@ -16,6 +16,7 @@ public class Note implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     @Column(name = "created_at")
