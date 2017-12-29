98555a56f6e9ff76cb94ea47e6326b3349a0e16e
Dan White <danjwh2@gmail.com>
2017-12-29 16:13:29 -0500

    Added User class
    (cherry picked from commit 33f23ff)

---
+++ src/main/java/com/example/myapp/domain/User.java
@@ -0,0 +1,105 @@
+package com.example.myapp.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(Date createdAt, String username, String password, String firstName, String lastName) {
+        this(null, createdAt, null, username, password, firstName, lastName);
+    }
+
+    public User(Long id, Date createdAt, Date lastUpdated, String username, String password, String firstName, String lastName) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.lastUpdated = lastUpdated;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
