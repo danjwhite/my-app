2dc76343cb35fb374cc978d5028c53a45a27076f
Dan White <danjwh2@gmail.com>
2018-01-02 20:47:25 -0500

    Removed security classes

--- src/main/java/com/example/myapp/config/SecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.example.myapp.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
