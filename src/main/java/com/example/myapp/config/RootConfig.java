91789b2d8241e9b32fca648e466db5ae34e40426
Dan White <danjwh2@gmail.com>
2018-01-04 17:35:15 -0500

    Added initial Spring Boot config files

--- src/main/java/com/example/myapp/config/RootConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapp.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.example.myapp")
-public class RootConfig {
-}
