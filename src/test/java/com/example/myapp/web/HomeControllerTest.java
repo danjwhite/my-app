b851b54601bd951977343e323f45fd70bd862ee5
Dan White <danjwh2@gmail.com>
2018-01-06 12:27:33 -0500

    Updated HomeControllerTest to use @Autowired

--- src/test/java/com/example/myapp/web/HomeControllerTest.java
+++ src/test/java/com/example/myapp/web/HomeControllerTest.java
@@ -7,6 +7,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,7 +18,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @TestPropertySource("classpath:application-test.properties")
 public class HomeControllerTest {
 
-    @Mock
+    @Autowired
     private HomeController homeController;
 
     @Test
