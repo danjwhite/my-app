b078bb9293f280d589d883a154936ac4e94c0f95
Dan White <danjwh2@gmail.com>
2018-01-07 11:09:59 -0500

    Updated the test data file

--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -1,12 +1,3 @@
-drop table if exists note;
-
-create table note (
-  id int auto_increment primary key,
-  created_at timestamp not null default current_timestamp,
-  title varchar(140) not null,
-  body varchar(5000) not null
-);
-
 insert into note(title, body) values
   ('Title', 'Body'),
   ('Title', 'Body'),
