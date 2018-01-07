5c10ef980485360da068a5c45283f87ae0d13955
Dan White <danjwh2@gmail.com>
2018-01-07 16:13:57 -0500

    Removed an unused import statement

--- src/main/java/com/example/myapp/web/UserRegistrationController.java
+++ src/main/java/com/example/myapp/web/UserRegistrationController.java
@@ -6,7 +6,6 @@ import com.example.myapp.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
