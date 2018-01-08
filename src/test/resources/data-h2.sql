abde61a99dac599da12fec3c444c584692718e76
Dan White <danjwh2@gmail.com>
2018-01-07 21:54:25 -0500

    Removed some test data

--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -11,16 +11,3 @@ insert into note (title, body) values
   ('Title', 'Body'),
   ('Title', 'Body'),
   ('Title', 'Body');
-
-insert into user (first_name, last_name, username, password, active) values
-  ('User1', 'Test', 'admin', '$2y$10$ssxSU/nxbDFVMrLzc4/cZ.9yokHPImaezdoYrz9Hcr.2BzD12YnR6', 1),
-  ('User2', 'Test', 'user', '$2y$10$ssxSU/nxbDFVMrLzc4/cZ.9yokHPImaezdoYrz9Hcr.2BzD12YnR6', 1);
-
-insert into role (type) values
-  ('ROLE_USER'),
-  ('ROLE_ADMIN');
-
-insert into user_role (user_id, role_id) values
-  (1, 1),
-  (1, 2),
-  (2, 1);
