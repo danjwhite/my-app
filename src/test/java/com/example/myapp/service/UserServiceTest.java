f0df23b668807b8a88e871ecd2a458a319561960
Dan White <danjwh2@gmail.com>
2018-01-13 09:36:27 -0500

    Added some comments to testUpdate() in UserServiceTest

--- src/test/java/com/example/myapp/service/UserServiceTest.java
+++ src/test/java/com/example/myapp/service/UserServiceTest.java
@@ -120,6 +120,7 @@ public class UserServiceTest {
     @Transactional
     public void testUpdate() {
 
+        // Get user.
         User user = userService.findById(3L);
         String firstName = user.getFirstName();
         String lastName = user.getLastName();
@@ -127,27 +128,37 @@ public class UserServiceTest {
         String password = user.getPassword();
         Set<Role> roles = new HashSet<>(user.getRoles());
 
+        // Declare updated password.
         String updatedPassword = "password456";
 
+        // Declare updated roles.
         Set<Role> updatedRoles = user.getRoles();
         updatedRoles.add(roleService.findByType("ROLE_ADMIN"));
 
+        // Set user password and roles.
         user.setPassword(updatedPassword);
         user.setRoles(updatedRoles);
 
+        // Assert the user count.
         assertEquals(4, userService.count());
 
+        // Update user and retrieve updated user.
         userService.update(user);
         User updatedUser = userService.findById(3L);
 
+        // Assert the user count.
         assertEquals(4, userService.count());
+
+        // Assert that non-updated fields remain the same.
         assertEquals(firstName, updatedUser.getFirstName());
         assertEquals(lastName, updatedUser.getLastName());
         assertEquals(username, updatedUser.getUsername());
 
+        // Assert that user password and roles were updated.
         assertTrue(bCryptPasswordEncoder.matches(updatedPassword, updatedUser.getPassword()));
         assertEquals(updatedRoles, updatedUser.getRoles());
 
+        // Assert that original password and roles do not match the updated ones.
         assertFalse(bCryptPasswordEncoder.matches(password, updatedUser.getPassword()));
         assertNotEquals(roles, updatedUser.getRoles());
     }
