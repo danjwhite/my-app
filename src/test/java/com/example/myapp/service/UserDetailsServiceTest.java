2d2e6ed45ea94d4a41c1ca42ee345b3191cca1fb
Dan White <danjwh2@gmail.com>
2018-01-13 15:18:58 -0500

    Added UserDetailsServiceTest class

---
+++ src/test/java/com/example/myapp/service/UserDetailsServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.myapp.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserDetailsServiceTest {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Test
+    @Transactional
+    public void testLoadUserByUsername() {
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername("mjones");
+
+        assertEquals("mjones", userDetails.getUsername());
+        assertEquals("$2a$10$.E7RjddSYnrH4iL49IFiPectcHJCFpHAIRyRAbf3kX4q4lsl6EYDS", userDetails.getPassword());
+        assertEquals(2, userDetails.getAuthorities().size());
+        assertTrue(userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_USER")));
+        assertTrue(userDetails.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN")));
+    }
+}
