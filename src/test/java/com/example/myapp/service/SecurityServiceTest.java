f8f5a7d99c6752b7678ef2dc655f8ddf405fef62
Dan White <danjwh2@gmail.com>
2018-01-14 14:46:09 -0500

    Revised SecurityServiceTest class

--- src/test/java/com/example/myapp/service/SecurityServiceTest.java
+++ src/test/java/com/example/myapp/service/SecurityServiceTest.java
@@ -2,10 +2,10 @@ package com.example.myapp.service;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Ignore
 public class SecurityServiceTest {
 
 }
