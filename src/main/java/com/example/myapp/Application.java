db2824ce2b88fddcc806f548f7d699b353b59f61
Dan White <danjwh2@gmail.com>
2018-01-14 13:48:44 -0500

    Added @ComponentScan to main Application class

--- src/main/java/com/example/myapp/Application.java
+++ src/main/java/com/example/myapp/Application.java
@@ -2,8 +2,10 @@ package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.myapp")
 public class Application {
 
     public static void main(String[] args) {
