b0ec4af571b8a1e4dc34a30b5b3f475a0ab8bee4
Dan White <danjwh2@gmail.com>
2018-01-17 20:38:18 -0500

    Added /resources/** to antMatchers() for HTTP security

--- src/main/java/com/example/myapp/config/SecurityConfig.java
+++ src/main/java/com/example/myapp/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/register").permitAll()
+                    .antMatchers("/resources/**", "/register").permitAll()
                     .anyRequest().authenticated()
                     .and()
                 .formLogin()
