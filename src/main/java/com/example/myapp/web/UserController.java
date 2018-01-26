f7ad124ceffddfa44276ce574f5bfc050d2681db
Dan White <danjwh2@gmail.com>
2018-01-25 22:04:18 -0500

    Added logout method to UserController

--- src/main/java/com/example/myapp/web/UserController.java
+++ src/main/java/com/example/myapp/web/UserController.java
@@ -3,11 +3,17 @@ package com.example.myapp.web;
 import com.example.myapp.domain.User;
 import com.example.myapp.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/")
@@ -27,5 +33,15 @@ public class UserController {
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
         return "loginForm";
+    }
+
+    @RequestMapping(value = "logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirectL:/login?logout";
     }
 }
