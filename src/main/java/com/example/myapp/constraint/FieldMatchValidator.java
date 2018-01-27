baaff359bf76566a080f56448e6cd62de70792cf
Dan White <danjwh2@gmail.com>
2018-01-27 12:51:55 -0500

    Updated FieldMatchValidator to add constraint violation message to the bean property

--- src/main/java/com/example/myapp/constraint/FieldMatchValidator.java
+++ src/main/java/com/example/myapp/constraint/FieldMatchValidator.java
@@ -18,12 +18,25 @@ public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Obje
 
     @Override
     public boolean isValid(final Object value, final ConstraintValidatorContext context) {
+        boolean fieldsMatch;
         try {
             final Object firstObject = BeanUtils.getProperty(value, firstFieldName);
             final Object secondObject = BeanUtils.getProperty(value, secondFieldName);
 
-            return (firstObject == null && secondObject == null) || (firstObject != null && firstObject.equals(secondObject));
+            String errorMessage = "Fields must match";
+
+            fieldsMatch = (firstObject == null && secondObject == null) || (firstObject != null && firstObject.equals(secondObject));
+
+            if (!fieldsMatch) {
+                context.disableDefaultConstraintViolation();
+                context.buildConstraintViolationWithTemplate(errorMessage).addPropertyNode(secondFieldName).addConstraintViolation();
+
+                return false;
+            }
+
+            return true;
         } catch (final Exception ignore) {
+            // Ignore
         }
 
         return true;
