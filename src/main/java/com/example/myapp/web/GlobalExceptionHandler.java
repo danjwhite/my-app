a4fce1a3b7035183bf45481774da8512155b52ac
Dan White <danjwh2@gmail.com>
2018-02-10 11:11:17 -0500

    Added handleError404() to GlobalExceptionHandler

--- src/main/java/com/example/myapp/web/GlobalExceptionHandler.java
+++ src/main/java/com/example/myapp/web/GlobalExceptionHandler.java
@@ -1,7 +1,18 @@
 package com.example.myapp.web;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = ResourceNotFoundException.class)
+    public ModelAndView handleError404() {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("errorTitle", "404: Resource Not Found");
+        modelAndView.addObject("errorDescription", "The requested resource could not be found.");
+
+        return modelAndView;
+    }
 }
