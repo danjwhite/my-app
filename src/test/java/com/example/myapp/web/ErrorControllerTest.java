88f6dbb06129174e8b5a6a2d81368342f2404737
Dan White <danjwh2@gmail.com>
2018-02-12 19:56:30 -0500

    Optimized imports for ErrorControllerTest

--- src/test/java/com/example/myapp/web/ErrorControllerTest.java
+++ src/test/java/com/example/myapp/web/ErrorControllerTest.java
@@ -13,10 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
