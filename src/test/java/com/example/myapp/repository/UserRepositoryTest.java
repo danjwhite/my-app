82396c658c2f6668c54d76f6f1287dd20670aabe
Dan White <danjwh2@gmail.com>
2019-10-02 22:48:37 -0400

    Updated UserRepositoryTest to user builder

--- src/test/java/com/example/myapp/repository/UserRepositoryTest.java
+++ src/test/java/com/example/myapp/repository/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.myapp.repository;
 
+import com.example.myapp.builder.entity.UserBuilder;
 import com.example.myapp.domain.Role;
 import com.example.myapp.domain.RoleType;
 import com.example.myapp.domain.User;
@@ -13,15 +14,16 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import javax.persistence.EntityManager;
+import java.util.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
 public class UserRepositoryTest {
+
+    @Autowired
+    private EntityManager entityManager;
 
     @Autowired
     private UserRepository userRepository;
@@ -33,6 +35,9 @@ public class UserRepositoryTest {
     @Transactional
     @Rollback
     public void countShouldReturnExpectedResult() {
+        newUser("user1");
+        newUser("user2");
+
         Assert.assertEquals(2, userRepository.count());
     }
 
@@ -40,138 +45,79 @@ public class UserRepositoryTest {
     @Transactional
     @Rollback
     public void findAllShouldReturnExpectedResult() {
-        Assert.assertEquals(2, ((List<User>) userRepository.findAll()).size());
+        List<User> users = Arrays.asList(newUser("user1"), newUser("user2"));
+
+        List<User> result = (List<User>) userRepository.findAll();
+        Assert.assertEquals(users, result);
     }
 
     @Test
     @Transactional
     @Rollback
-    @SuppressWarnings("Duplicates")
     public void findByIdShouldReturnExpectedResult() {
-        Optional<User> user = userRepository.findById(1L);
+        User user1 = newUser("user");
+        newUser("user2");
 
-        Role userRole = roleRepository.findByType(RoleType.ROLE_USER);
-        Role adminRole = roleRepository.findByType(RoleType.ROLE_ADMIN);
-        Set<Role> roles = new HashSet<>();
-        roles.add(userRole);
-        roles.add(adminRole);
-
-        Assert.assertTrue(user.isPresent());
-        Assert.assertEquals(1L, user.get().getId().longValue());
-        Assert.assertEquals("Michael", user.get().getFirstName());
-        Assert.assertEquals("Jones", user.get().getLastName());
-        Assert.assertEquals("mjones", user.get().getUsername());
-        Assert.assertEquals("$2a$10$.E7RjddSYnrH4iL49IFiPectcHJCFpHAIRyRAbf3kX4q4lsl6EYDS", user.get().getPassword());
-        Assert.assertEquals(roles, user.get().getRoles());
+        Optional<User> result = userRepository.findById(user1.getId());
+        Assert.assertTrue(result.isPresent());
+        Assert.assertEquals(user1, result.get());
     }
 
     @Test
     @Transactional
     @Rollback
-    @SuppressWarnings("Duplicates")
     public void findByUsernameShouldReturnExpectedResult() {
-        User user = userRepository.findByUsername("mjones");
+        User user1 = newUser("user1");
+        newUser("user2");
 
-        Role userRole = roleRepository.findByType(RoleType.ROLE_USER);
-        Role adminRole = roleRepository.findByType(RoleType.ROLE_ADMIN);
-        Set<Role> roles = new HashSet<>();
-        roles.add(userRole);
-        roles.add(adminRole);
-
-        Assert.assertEquals(1L, user.getId().longValue());
-        Assert.assertEquals("Michael", user.getFirstName());
-        Assert.assertEquals("Jones", user.getLastName());
-        Assert.assertEquals("mjones", user.getUsername());
-        Assert.assertEquals("$2a$10$.E7RjddSYnrH4iL49IFiPectcHJCFpHAIRyRAbf3kX4q4lsl6EYDS", user.getPassword());
-        Assert.assertEquals(roles, user.getRoles());
+        User result = userRepository.findByUsername("user1");
+        Assert.assertEquals(user1, result);
     }
 
-    // TODO: Break down into smaller tests
     @Test
     @Rollback
     @Transactional
-    public void saveShouldSetExpectedFieldsforAdd() {
+    public void saveShouldSetExpectedFieldsForAdd() {
+        final String firstName = "Mike";
+        final String lastName = "Jones";
+        final String username = "mjones";
+        final Set<Role> roles = Collections.singleton(roleRepository.findByType(RoleType.ROLE_USER));
 
-        User user = new User();
+        User user = UserBuilder.givenUser().withFirstName(firstName)
+                .withLastName(lastName)
+                .withUsername(username)
+                .withPassword("test123")
+                .withRoles(roles)
+                .build();
 
-        Role userRole = roleRepository.findByType(RoleType.ROLE_USER);
-        Set<Role> roles = new HashSet<>();
-        roles.add(userRole);
+        userRepository.save(user);
+        Assert.assertNotNull(user.getId());
 
-        user.setFirstName("Joseph");
-        user.setLastName("Manning");
-        user.setUsername("jmanning");
-        user.setPassword("$2a$10$.E7RjddSYnrH4iL49IFiPectcHJCFpHAIRyRAbf3kX4q4lsl6EYDS");
-        user.setRoles(roles);
-
-        Assert.assertEquals(2, userRepository.count());
-
-        User savedUser = userRepository.save(user);
-
-        Assert.assertEquals(3, userRepository.count());
-        Assert.assertEquals(3L, savedUser.getId().longValue());
-        Assert.assertEquals("Joseph", savedUser.getFirstName());
-        Assert.assertEquals("Manning", savedUser.getLastName());
-        Assert.assertEquals("jmanning", savedUser.getUsername());
-        Assert.assertEquals("$2a$10$.E7RjddSYnrH4iL49IFiPectcHJCFpHAIRyRAbf3kX4q4lsl6EYDS", user.getPassword());
-        Assert.assertEquals(roles, savedUser.getRoles());
-    }
-
-    // TODO: Break down into smaller tests
-    @Test
-    @Transactional
-    @Rollback
-    public void saveShouldSetExpectedFieldsForUpdate() {
-
-        // Get user and save original fields.
-        Optional<User> user = userRepository.findById(2L);
-        Assert.assertTrue(user.isPresent());
-        String firstName = user.get().getFirstName();
-        String lastName = user.get().getLastName();
-        String username = user.get().getUsername();
-        String password = user.get().getPassword();
-        Set<Role> roles = new HashSet<>(user.get().getRoles());
-
-        // Declare updated roles.
-        Set<Role> updatedRoles = user.get().getRoles();
-        updatedRoles.add(roleRepository.findByType(RoleType.ROLE_ADMIN));
-
-        // Set user roles.
-        user.get().setRoles(updatedRoles);
-
-        Assert.assertEquals(2, userRepository.count());
-
-        // Update and retrieve user.
-        userRepository.save(user.get());
-        Optional<User> updatedUser = userRepository.findById(2L);
-
-        Assert.assertTrue(updatedUser.isPresent());
-        Assert.assertEquals(2, userRepository.count());
-        Assert.assertEquals(firstName, updatedUser.get().getFirstName());
-        Assert.assertEquals(lastName, updatedUser.get().getLastName());
-        Assert.assertEquals(username, updatedUser.get().getUsername());
-        Assert.assertEquals(password, updatedUser.get().getPassword());
-
-        Assert.assertEquals(updatedRoles, updatedUser.get().getRoles());
-
-        Assert.assertNotEquals(roles, updatedUser.get().getRoles());
+        Optional<User> savedUser = userRepository.findById(user.getId());
+        Assert.assertTrue(savedUser.isPresent());
+        Assert.assertEquals(firstName, savedUser.get().getFirstName());
+        Assert.assertEquals(lastName, savedUser.get().getLastName());
+        Assert.assertEquals(username, savedUser.get().getUsername());
+        Assert.assertEquals(roles, savedUser.get().getRoles());
     }
 
     @Test
     @Transactional
     @Rollback
-    @SuppressWarnings("Duplicates")
     public void deleteShouldDeleteExpectedUser() {
-
-        // Get user to delete.
-        User user = userRepository.findByUsername("mjones");
-
-        Assert.assertEquals(2, userRepository.count());
-        Assert.assertNotNull(user);
+        User user = newUser("mjones");
+        Assert.assertTrue(userRepository.findById(user.getId()).isPresent());
 
         userRepository.delete(user);
+        Assert.assertFalse(userRepository.findById(user.getId()).isPresent());
+    }
 
-        Assert.assertEquals(1, userRepository.count());
-        Assert.assertNull(userRepository.findByUsername("mjones"));
+    private User newUser(String username) {
+        return UserBuilder.givenUser(entityManager).withFirstName("Test")
+                .withLastName("User")
+                .withUsername(username)
+                .withPassword("test123")
+                .withRoles(Collections.singleton(roleRepository.findByType(RoleType.ROLE_USER)))
+                .build();
     }
 }
