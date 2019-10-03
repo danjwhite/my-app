0ebcf0b132be87315331a362f80f65d3303fe3fd
Dan White <danjwh2@gmail.com>
2019-10-03 11:38:50 -0400

    Moved RoleConverter out of the main application class into a new WebConfig class

--- src/main/java/com/example/myapp/Application.java
+++ src/main/java/com/example/myapp/Application.java
@@ -1,24 +1,10 @@
 package com.example.myapp;
 
-import com.example.myapp.converter.RoleConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-@ComponentScan("com.example.myapp")
-public class Application extends WebMvcConfigurerAdapter {
-
-    @Autowired
-    private RoleConverter roleConverter;
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(roleConverter);
-    }
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
